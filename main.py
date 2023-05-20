9fbdae46b00647115384dc9e8e1a85d49196c37f
jkrrrr <christopherharland@protonmail.com>
2023-05-20 16:54:26 +0100

    Revised file structure

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import pandas as pd
+import logging
+
 def main():
     print("Starting")
+    
+    fileName="colour_info.xlsx"
+    
+    logging.basicConfig(filename="file.log",
+                   format="%(asctime)s %(message)s",
+                   filemode="w")
+    logger = logging.getLogger()
+
+    logger.setLevel(logging.INFO)
+        
+    excel = pd.read_excel(fileName)
+    logger.info("Read excel file ", fileName)
+    print(excel)
 
 if __name__ == '__main__':
     main()
